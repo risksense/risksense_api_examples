b7f811cdcf675c4d21e72e87f3b419d8b7134de4
Burr Webb <burr.webb@risksense.com>
2019-08-09 15:08:54 -0600

    adding more Apache-2.0 licensing information

--- examples/python/single client/get_hosts.py
+++ examples/python/single client/get_hosts.py
@@ -1,13 +1,12 @@
-""" ******************************************************************
-
-Name        : get_hosts.py
-Description : Retrieves a list of hosts with a criticality of "5"
-              from all clients associated with a user via the
-              RiskSense REST API.
-Copyright   : (c) RiskSense, Inc.
-License     : Apache-2.0
-
-****************************************************************** """
+""" *******************************************************************************************************************
+|
+|  Name        : get_hosts.py
+|  Description : Retrieves a list of hosts with a criticality of "5" from all clients associated with a user via the
+                 RiskSense REST API.
+|  Copyright   : (c) RiskSense, Inc.
+|  License     : Apache-2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
+|
+******************************************************************************************************************* """
 
 import json
 import os
@@ -166,3 +165,19 @@ def main():
 #  Execute the Script
 if __name__ == "__main__":
     main()
+
+"""
+   Copyright 2019 RiskSense, Inc.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
